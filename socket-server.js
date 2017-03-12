b149916d850af9081cd9a2a79362188be4ee5e03
Vjachelsav Gusser <w.gusser@mail.ru>
2017-03-12 11:36:55 +0300

    broadcast code changes

--- socket-server.js
+++ socket-server.js
@@ -4,6 +4,11 @@ const socket_io = require('socket.io')();
 
 socket_io.on('connection', (socket) => {
     console.log('new client connected');
+
+    socket.on('code-change', (event) => {
+        // Broadcast this event
+        socket.broadcast.emit('other-user-change-code', event);
+    });
 });
 
 module.exports = socket_io;
